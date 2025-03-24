cb3c1189549ad52102f1f166bddd8fee88d75df5
Rafen19 <noboarabel@gmail.com>
2025-03-24 09:28:00 +0000

    Update app.js
    He cambiado el app.js que esta fuera de la carpeta

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-import "bootstrap";
-import "./style.css";
+// Arrays con las palabras para generar dominios
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let extensions = ['.com', '.net', '.us', '.io'];
 
+// Función para generar dominios
+function generateDomains() {
+    // Recorremos cada array con bucles anidados
+    for (let p of pronoun) {
+        for (let a of adj) {
+            for (let n of noun) {
+                for (let ext of extensions) {
+                    // Concatenamos las palabras y la extensión
+                    let domain = `${p}${a}${n}${ext}`;
+                    console.log(domain); // Mostramos el dominio en la consola
+                }
+            }
+        }
+    }
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// Llamamos a la función para generar los dominios
+generateDomains();
